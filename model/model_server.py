ccd8cb48af50663fdaf50f642c72103a68c15e3e
Anastasia Kulik <72699189+akukulik@users.noreply.github.com>
2024-12-19 00:50:38 +0300

    Update model_server.py

--- model/model_server.py
+++ model/model_server.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import torch
-from transformers import pipeline
+from fastapi import FastAPI # для создания веб-приложения
+from pydantic import BaseModel # для валидации данных запросов
+import torch  # для работы с нейросетями
+from transformers import pipeline   # для работы с предобученными моделями
 
+# Инициализация FastAPI-приложения
 app = FastAPI()
 
+
+# Настройка предобученной модели для классификации текста по степени депрессивности
+# Используется модель deproberta-large-depression из библиотеки Hugging Face.
+# Устройство выбирается автоматически: GPU (если доступно) или CPU.
 model = pipeline(
     'text-classification',
     'rafalposwiata/deproberta-large-depression',
@@ -12,10 +17,12 @@ model = pipeline(
 )
 
 
+# Ожидается, что запрос содержит поле "text", которое должно быть строкой.
 class PredictionRequest(BaseModel):
     text: str
 
 
+# Принимает запрос с текстом, обрабатывает его моделью и возвращает результат.
 @app.post("/predict")
 async def predict(request: PredictionRequest):
     result = model(request.text)
@@ -23,5 +30,5 @@ async def predict(request: PredictionRequest):
 
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn # сервер для запуска приложения
+    uvicorn.run(app, host="0.0.0.0", port=8000) # запуск приложения на порту 8000
